c3a2aba8da147a1a783ecaf87fbf11b4861eabb4
Mariana Mozzer Arantes <102536415+mariimozzer@users.noreply.github.com>
2025-04-08 09:03:43 -0300

    att cadastro de produtos, roteiro e estrutura

--- src/components/EstruturaArvore/EstruturaComponent.vue
+++ src/components/EstruturaArvore/EstruturaComponent.vue
@@ -7,8 +7,9 @@
     <i :class="caretIcon"></i>
     <span :class="classeProduto" class="tipo-produto"></span>
     <span class="item-description" @click.stop="mostrarDetalhes(item)" :title="item.desc ?? item.produto_desc">
-      UDO030242* - {{ item.desc ?? item.produto_desc }} <span v-if="item.qt && item.unidade"> - {{ item.qt }} {{
-        item.unidade }}</span></span>
+      UDO030242* - {{ item.desc ?? item.produto_desc }} <span v-if="item.qt && item.unidade"> - <b>{{ item.qt }} {{
+        item.unidade }}</b></span> <i class="bi bi-trash" @click.stop="confirmarRemocao(item)"
+        style="font-size: 15px; cursor: pointer; color: red; "></i></span>
   </div>
   <div v-if="isOpen" class="child-items">
     <EstruturaComponent v-for="(childItem, index) in itensTratados.filhos" :key="index"
@@ -18,8 +19,21 @@
       <AutoCompleteComponent @adicionarItem="adicionarItem" @abrirModalNovoItem="abrirModal" />
     </div>
   </div>
+  <!-- MODAL -->
+  <div class="modal-mask" v-if="modalConfirmacao" @click="fecharModalConfirmacao()">
+    <div class="jm margem" @click.stop>
+      <div class="alinha-centro">
+        <h3>Confirmar Remoção</h3>
+        <p>Tem certeza que deseja remover?</p>
+      </div>
+      <div class="submit direita">
+        <button @click="removerConfirmado">Remover</button>
+        <button @click="fecharModalConfirmacao" class="acao-secundaria">Cancelar</button>
+      </div>
+    </div>
+  </div>
+  <!--END MODAL -->
 </template>
-
 <script>
 import AutoCompleteComponent from '@/components/AutoComplete/AutoCompleteComponent.vue';
 export default {
@@ -35,6 +49,9 @@ export default {
     return {
       itensTratados: this.item,
       isOpen: this.iniciarAberto,
+      modalConfirmacao: false,
+      itemParaRemocao: null,
+
     };
   },
   computed: {
@@ -66,6 +83,25 @@ export default {
     },
   },
   methods: {
+    confirmarRemocao(item) {
+      this.itemParaRemocao = item;
+      this.modalConfirmacao = true;
+    },
+    fecharModalConfirmacao() {
+      this.modalConfirmacao = false;
+      this.itemParaRemocao = null;
+    },
+    removerConfirmado() {
+      if (this.itemParaRemocao) {
+        if (this.itemParaRemocao.id === this.item.id) {
+          this.$emit('removerItem', this.item.id);
+        } else {
+          this.removerItem(this.itemParaRemocao.id);
+        }
+      }
+      this.fecharModalConfirmacao();
+    }
+    ,
     removerItemPai(id) {
       this.$emit('removerItem', id)
     },
@@ -86,7 +122,6 @@ export default {
   },
 };
 </script>
-
 <style scoped>
 .bi-trash:hover {
   transition: all 100ms linear;
