0f9cd9c8b41e50f074122b7945e0d74b63fd55c8
rdd2 <rauldd2008@gmail.com>
2025-10-19 20:50:36 -0300

    nova ficha comentada

--- src/components/MenuLateral/MenuLateralComponent.vue
+++ src/components/MenuLateral/MenuLateralComponent.vue
@@ -24,6 +24,7 @@
       <router-link to="/gabarito" active-class="ativo">Gabaritos</router-link>
       <router-link to="/configurar" active-class="ativo">Campos por Família</router-link>
       <router-link to="/codificacoes" active-class="ativo">Codificação de Serviços</router-link>
+      <!-- <router-link to="/novaFicha">Nova Ficha de Produtos</router-link> -->
       <!-- <router-link to="/campos" active-class="ativo">Cadastro de Campos</router-link> -->
       <!-- <router-link to="/camposListar" active-class="ativo">Campos Listar</router-link> -->
       <!-- <router-link v-if="permissoes.includes(97)" to="/produtos" active-class="ativo">Produtos em      
