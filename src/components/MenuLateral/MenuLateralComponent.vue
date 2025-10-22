b4bbac1274d407745496e46f2d0d7b740fa051f7
Mariana Mozzer Arantes <102536415+mariimozzer@users.noreply.github.com>
2025-10-22 08:53:16 -0300

    att

--- src/components/MenuLateral/MenuLateralComponent.vue
+++ src/components/MenuLateral/MenuLateralComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <aside>
     <div class="titulo margem efeito">
-      <div class="m-icone"><a href="#" class="icone-menu" title="Menu" onclick="document.getElementById('main').classList.toggle('esconde');return false"></a></div>
+      <div class="m-icone"><a href="#" class="icone-menu" title="Menu"
+          onclick="document.getElementById('main').classList.toggle('esconde');return false"></a></div>
     </div>
     <!-- 95 Montagem | Fluxo de Venda
 96 Montagem | Portfólio de Produtos
@@ -24,7 +25,7 @@
       <router-link to="/gabarito" active-class="ativo">Gabaritos</router-link>
       <router-link to="/configurar" active-class="ativo">Campos por Família</router-link>
       <router-link to="/codificacoes" active-class="ativo">Codificação de Serviços</router-link>
-      <!-- <router-link to="/novaFicha">Nova Ficha de Produtos</router-link> -->
+      <router-link to="/novaFicha">Nova Ficha de Produtos</router-link>
       <!-- <router-link to="/campos" active-class="ativo">Cadastro de Campos</router-link> -->
       <!-- <router-link to="/camposListar" active-class="ativo">Campos Listar</router-link> -->
       <!-- <router-link v-if="permissoes.includes(97)" to="/produtos" active-class="ativo">Produtos em      
