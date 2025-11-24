03dc1668e13984c29e0c82c930d974dd88d565b7
Mariana Mozzer Arantes <102536415+mariimozzer@users.noreply.github.com>
2025-11-24 13:59:30 -0300

    inicio serviços

---
+++ src/views/Serviços/CatalogoServicos.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <div class="titulo">
+            <div class="margem container">
+                <div class="m-icone direita">
+                    <div class="pesquisa">
+                        <input type="text" placeholder="Pesquisar serviço" />
+                        <a class="icone-pesquisa" title="Pesquisar"></a>
+                    </div>
+                </div>
+                <h2>Catálogo de Serviços</h2>
+            </div>
+        </div>
+    </section>
+</template>
